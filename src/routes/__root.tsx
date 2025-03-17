c2821f65e14c311fcf5a07d90ba2572f9b0996be
Duncan <duncan.painter1@gmail.com>
2025-03-17 13:49:43 +0000

    error component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import {
+  createRootRouteWithContext,
+  Link,
+  Outlet,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthContext } from "@/components/auth/AuthContext";
@@ -9,6 +13,18 @@ interface MyRouterContext {
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
+  notFoundComponent: () => {
+    return (
+      <div className="h-full w-full text-center flex flex-col items-center gap-4 justify-center">
+        <div>
+          <h1 className="h-full text-3xl">404 NOT FOUND</h1>
+        </div>
+        <Link to="/" className="underline">
+          home
+        </Link>
+      </div>
+    );
+  },
   component: () => (
     <>
       <Outlet />
