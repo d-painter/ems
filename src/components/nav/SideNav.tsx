74416f975fe0b9800a48de8a2f10a7880b2a9d56
Duncan <duncan.painter1@gmail.com>
2025-04-28 17:24:39 +0100

    google integration

--- src/components/nav/SideNav.tsx
+++ src/components/nav/SideNav.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
 import LogoutButton from "./LogoutButton";
 import { UserAuth } from "../auth/AuthContext";
-import { UserCircle } from "lucide-react";
 
 export default function SideNav({ children }: { children: ReactNode }) {
   const { session } = UserAuth();
@@ -9,11 +8,8 @@ export default function SideNav({ children }: { children: ReactNode }) {
     <div className="w-44 lg:w-56 hidden shrink-0 md:flex-col h-full md:flex p-4 border-r border-r-primary">
       <div className="my-auto">{children}</div>
       <div className="mt-auto">
-        <div className="w-full max-w-54">
-          <div className="flex flex-row gap-1 items-center">
-            <UserCircle size={18} />
-            <p className="text-base text-wrap">{session?.user?.email}</p>
-          </div>
+        <div className="w-full max-w-54 space-y-1">
+          <p className="text-xs text-wrap">{session?.user?.email}</p>
           <LogoutButton />
         </div>
       </div>
