79cfeb814de21610dc5588f6ea192169dd99560d
Duncan <duncan.painter1@gmail.com>
2026-01-22 16:42:04 +0000

    build fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import cors from "cors";
-import projects from "./projects.ts";
+import projects from "./projects.js";
 
 const app = express();
 app.use(cors());
